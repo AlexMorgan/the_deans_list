909e8f9133857bf30e22d78d4e76f5e24611fee3
AlexMorgan <alexmorgan.am@gmail.com>
2014-08-06 19:04:36 -0400

    added stripe out of the box with minimal functionality

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,29 @@
 class ChargesController < ApplicationController
+  def new
+  end
+
+  def create
+    # Amount in cents
+    @amount = 1500
+
+    customer = Stripe::Customer.create(
+      :email => 'example@stripe.com',
+      :card  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Calendar Purchased',
+      :currency    => 'usd'
+    )
+
+    # custome redirect - Get rid of this if you want to touch the charges#create page
+    flash[:notice] = "Your order is being processed"
+    redirect_to root_path
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path
+  end
 end
