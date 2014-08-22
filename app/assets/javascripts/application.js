6ce63b7ce5e476ea3db40a15544aa99842ef4cca
AlexMorgan <alexmorgan.am@gmail.com>
2014-08-22 10:42:46 -0400

    Changed back to original layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require flipclock
 //= require jquery.BlackAndWhite
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :skip => :registrations
-  root "pages#countdown"
+  root "pages#home"
 
   resources :girls
   resources :contact_forms, only: [:new, :create]
