5bb9d510cad4a4d826e1f0227345031957cb0075
AlexMorgan <alexmorgan.am@gmail.com>
2014-09-02 15:01:04 -0400

    Changed redirect path after successful sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    girls_path
+  end
 end
