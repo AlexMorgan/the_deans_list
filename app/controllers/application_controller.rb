c88b78575a479dcd7760f5f1845b19df4618e0e6
AlexMorgan <alexmorgan.am@gmail.com>
2014-09-03 02:16:35 -0400

    Started to add profiles for users to track sales, need to figure out how to get names to save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     girls_path
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:password, :password_confirmation, :current_password, :first_name, :last_name)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :skip => :registrations
+  devise_for :users, :controllers => { :registrations => "registrations" }
   root "pages#home"
-
+  resources :users, only: [:index, :show]
   resources :girls
   resources :locations
   resources :contact_forms, only: [:new, :create]
